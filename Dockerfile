728dfd7a35ee36c1239c5d56105efd4603004335
Dhiraj Himani <dhirajhimani@gmail.com>
2023-03-14 12:09:08 +0100

    working

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:lts-alpine
+
+WORKDIR /app
+COPY package.json /app
+RUN yarn install
+COPY . /app
+
+EXPOSE 3000
+
+CMD ["node", "dist/server.js"]
---
+++ scripts/wait-for.js
@@ -0,0 +1,12 @@
+// const waitPort = require('wait-port');
+//
+// async function wait() {
+//   const [host, port] = ['amqp://guest:guest@rabbitmq','5672'];
+//   await waitPort({
+//     host,
+//     port: Number(port),
+//     timeout: 10000
+//   });
+// }
+//
+// wait();
